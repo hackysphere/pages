84e51b39a8118a1eb9abe6569af96f5a35f58205
hackysphere <53366983+hackysphere@users.noreply.github.com>
2025-06-16 23:53:16 -0600

    Add Open Graph support and sitemap creation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://hcks.dev/",
+  site: "https://hcks.dev/",
+
+  i18n: {
+      locales: ["en"],
+      defaultLocale: "en"
+  },
+
+  integrations: [sitemap({
+    filter: (page) =>
+      page !== "https://hcks.dev/404/" &&
+      page !== "https://hcks.dev/migrate/"
+  })],
 });
