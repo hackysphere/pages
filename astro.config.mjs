79855bcb4211931b748f7377add34656be1db30f
hackysphere <53366983+hackysphere@users.noreply.github.com>
2025-09-06 22:06:53 -0600

    fix code blocks and use mdx for future addition of images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,9 +13,15 @@ export default defineConfig({
       defaultLocale: "en"
   },
 
+  markdown: {
+    shikiConfig: {
+      theme: "catppuccin-mocha"
+    }
+  },
+
   integrations: [sitemap({
     filter: (page) =>
       page !== "https://hcks.dev/404/" &&
       page !== "https://hcks.dev/migrate/"
-  })],
+  }), mdx()],
 });
